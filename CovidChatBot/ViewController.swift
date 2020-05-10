12fe3ab18475a2907e05d4c1980d7ce11ea517bd
SUBNARA <subhashini.narayanaswamy@daimler.com>
2020-05-10 18:39:18 +0530

    Initial commit

---
+++ CovidChatBot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidChatBot
+//
+//  Created by Narayanaswamy, Subhashini (623) on 10/05/20.
+//  Copyright © 2020 Narayanaswamy, Subhashini (623). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
