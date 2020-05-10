87d71bc97acb64b12bd3caf3902f9ccc56ddedd8
SUBNARA <subhashini.narayanaswamy@daimler.com>
2020-05-10 20:52:38 +0530

    Table view addtion and other UI changes

---
+++ CovidChatBot/PaddedLabel.swift
@@ -0,0 +1,37 @@
+//
+//  PaddedLabel.swift
+//  CovidChatBot
+//
+//  Created by Narayanaswamy, Subhashini (623) on 10/05/20.
+//  Copyright © 2020 Narayanaswamy, Subhashini (623). All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class PaddedLabel: UILabel {
+
+    @IBInspectable var topInset: CGFloat = 5.0
+    @IBInspectable var bottomInset: CGFloat = 5.0
+    @IBInspectable var leftInset: CGFloat = 7.0
+    @IBInspectable var rightInset: CGFloat = 7.0
+
+    override func drawText(in rect: CGRect) {
+        let insets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + leftInset + rightInset,
+                      height: size.height + topInset + bottomInset)
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- CovidChatBot/ViewController.swift
+++ CovidChatBot/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 import CSVImporter
 
+enum SenderType {
+    case user
+    case computer
+}
+
 class ViewController: UIViewController {
 
     var recordsArray = Array<Any>()
+    var data = Array<[SenderType: Any]>()
+    
     @IBOutlet weak var loaderView: LoaderView!
-
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         loadCSVFile()
+        data.append([.computer: "Hi! I'm your chatbot. What is your name? Hi! I'm your chatbot. What is your name? Hi! I'm your chatbot. What is your name? Hi! I'm your chatbot. What is your name?"])
+        data.append([.user: "Hi! I'm Subhashini. Just trying some new stuff!!!"])
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.allowsSelection = false
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
     }
     
     func loadCSVFile() {
@@ -48,5 +63,61 @@ class ViewController: UIViewController {
         }
     }
     
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let datum = data[indexPath.row]
+        if let userData = datum[.user] as? String {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell") as? UserTableViewCell {
+                cell.messageLabel.text = userData
+                cell.setupUI()
+                return cell
+            }
+        }
+        
+        if let computerData = datum[.computer] as? String {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "ComputerCell") as? ComputerTableViewCell {
+                cell.messageLabel.text = computerData
+                cell.setupUI()
+                return cell
+            }
+        }
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+}
+
+
+class UserTableViewCell: UITableViewCell {
+    @IBOutlet weak var messageLabel: PaddedLabel!
+    
+    func setupUI() {
+        self.backgroundColor = .clear
+        messageLabel.layer.masksToBounds = true
+        messageLabel.layer.cornerRadius = 8.0
+    }
+}
+
+class ComputerTableViewCell: UITableViewCell {
+    @IBOutlet weak var messageLabel: PaddedLabel!
+    
+    func setupUI() {
+        self.backgroundColor = .clear
+        messageLabel.layer.masksToBounds = true
+        messageLabel.layer.cornerRadius = 8.0
+    }
 }
 
